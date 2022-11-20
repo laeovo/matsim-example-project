010664234211cefe7f5bf12c7c7c60335db0f6a7
Leo Vornberger <leo.vornberger@gmx.de>
2022-11-20 23:06:36 +0100

    Add 20221107

---
+++ src/main/java/org/matsim/project/Run20221107.java
@@ -0,0 +1,25 @@
+package org.matsim.project;
+
+import org.matsim.api.core.v01.Scenario;
+import org.matsim.core.config.Config;
+import org.matsim.core.config.ConfigUtils;
+import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.controler.Controler;
+import org.matsim.core.scenario.ScenarioUtils;
+
+public class Run20221107 {
+    public static void main(String[] args) {
+        Config config = ConfigUtils.loadConfig("scenarios/equil/config.xml");
+        Scenario scenario = ScenarioUtils.loadScenario(config);
+
+        Controler controler = new Controler(scenario);
+        final AbstractModule module = new AbstractModule() {
+            @Override public void install() {
+                this.bindMobsim().toInstance(null);
+            }
+        };
+
+        controler.addOverridingModule(module);
+        controler.run();
+    }
+}
